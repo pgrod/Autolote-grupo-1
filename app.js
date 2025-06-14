0ec3581f024a2f1b1c8983e44d607fab0ed64618
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-06-14 16:04:36 -0600

    jwt and bcrypt

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mysql=require('mysql2')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const app = express();
 const port=3000;
 
@@ -7,7 +9,7 @@ const port=3000;
 const pool=mysql.createPool({
     host:'localhost',
     user:'root',
-    password:'Coquien12!',
+    password:'admin123',
     database:'flota',
 })
 
